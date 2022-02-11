134f9c3d78af41b5efca28a4c0a9121014a980fd
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-02-10 20:34:48 -0800

    added PseudoCode to main.js

---
+++ Milestone-Project-1/main.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+// generate maze with walls 
+
+// maze will need walls character cannot walk through 
+
+// add player character
+
+// player character move function
+
+// player character attack function
+
+// player character lose on hit
+
+// player character win on touch treasure 
+
+// add enemy characters
+
+// enemy character obstacle
+
+// enemy character removal
+
+// add treasure 
+
+// connect touching treasure with win condition 
