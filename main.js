1e3c2aa9c5228e3dab58ab239f9c77605da644b2
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-02-20 10:35:40 -0800

    added toggle-wall to make the game longer

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const enemy2Div = document.querySelector("#enemy2")
 const enemy3Div = document.querySelector("#enemy3")
 const winMessage = document.querySelector("#win-message")
 const restartBtn = document.querySelector("#restart")
+const toggleWall = document.querySelector(".toggle-wall")
+const wallToggle = document.querySelector(".wall-toggle")
 
 // time variables 
 let startTime
@@ -25,8 +27,7 @@ function getEndTime(){
 }
 function startGame(){
     startTime = new Date();
-    // hides the start screen after the button is clicked
-    startDiv.classList.remove("show")
+    startDiv.classList.remove("show") // hides the start screen
 }
 function showWin(){
     winMessage.innerHTML = `You win! Your time ${elapsedTime} seconds!`
@@ -34,7 +35,7 @@ function showWin(){
 }
 function displayTimes(){
     timeDisplay.innerHTML = "Time Display"  // removes any times currently being displayed 
-    elapsedTimeStorage.forEach(time => {
+    elapsedTimeStorage.forEach(time => { // displays every time we have in our elaspedTimeStorage array as a new Li
     let timeli = document.createElement("li")
     timeli.textContent = `${time}`
     timeDisplay.append(timeli)
@@ -46,53 +47,65 @@ function resetPlayer(){
 }
 
 
+
     // Event listeners
     window.addEventListener("keypress",(e)=>{
-
+        // collosion checks and game start checks. Would like to figure out how to get these to work outside our event listener
         let checkMoveRight = wallsData.some(wall =>  wall.x === playerData.x + 1  && wall.y === playerData.y)
         let checkMoveLeft = wallsData.some(wall => wall.x === playerData.x - 1 && wall.y === playerData.y)
         let checkMoveDown = wallsData.some(wall => wall.x === playerData.x && wall.y === playerData.y + 1)
         let checkMoveUp = wallsData.some(wall => wall.x === playerData.x && wall.y === playerData.y - 1)
         let checkTreasure = treasureData.some(treasure => treasure.x === playerData.x && treasure.y === playerData.y)
+        let checkWallToggle = wallToggleData.some(wallToggle => wallToggle.x === playerData.x && wallToggle.y === playerData.y)
         let gameStarted = !startDiv.classList.contains("show")
         let gameRestarted = !winDiv.classList.contains("show")
-        let checkLose = enemiesData.some(enemy => enemy.x === playerData.x && enemy.y === playerData.y)
+        let checkEnemyCollision = enemiesData.some(enemy => enemy.x === playerData.x && enemy.y === playerData.y)
 
         // Simple if statements for our keypresses this can definitly be refactored 
         if(e.key === "s" && checkMoveDown === false && gameStarted && gameRestarted){
             playerData.y += 1 
             playerDiv.style.top = playerData.y *50 - 50 + "px";
-            if(checkLose === true){
+            if(checkEnemyCollision === true){
                 resetPlayer()
             }
         }
         if(e.key === "w" && checkMoveUp === false && gameStarted && gameRestarted){
              playerData.y -= 1 
              playerDiv.style.top = playerData.y * 50 - 50 + "px";
-             if(checkLose === true){
+             if(checkEnemyCollision === true){
                 resetPlayer()
             }
             }
         if(e.key === "d" && checkMoveRight === false && gameStarted && gameRestarted){
             playerData.x += 1 
-            playerDiv.style.left = (playerData.x - 9) * 50 - 50 + "px" ;// I had to subtract 9 from the initial player data because the player starts on x = 10 but the playerDiv's movement is relative to its original position
-            if(checkLose === true){
+            playerDiv.style.left = (playerData.x - 9) * 50 - 50 + "px" ;// subtract 9 from the initial player data because the player starts on x = 10 but the playerDiv's movement is relative to its original position
+            if(checkEnemyCollision === true){
                 resetPlayer()
             }
             }
         if(e.key === "a" && checkMoveLeft === false && gameStarted && gameRestarted){
             playerData.x -= 1 
             playerDiv.style.left = (playerData.x - 9) * 50 - 50 + "px";
-            if(checkLose === true){
+            if(checkEnemyCollision === true){
                 resetPlayer()
             }
             }
-
+        
+        if(checkWallToggle === true && wallsData.length === 149){
+            wallsData.pop()// removes the last two objects from our wallsData array allowing the player to move through the toggle wall 
+            wallsData.pop()
+            toggleWall.classList.add("hide")
+            wallToggle.classList.add("hide")
+        }
         if(checkTreasure === true){
             getEndTime()
             resetPlayer()
             showWin()
             displayTimes()
+            wallsData.push(toggleWallData[0])// pushes our toggle wall data back into wall data to prevent the player from going through it
+            wallsData.push(toggleWallData[1])
+            toggleWall.classList.remove("hide")
+            wallToggle.classList.remove("hide")
         }
     })
 
@@ -100,7 +113,6 @@ function resetPlayer(){
     // Start event listener also gets our start time and pushes the time to our timeStorage array 
     startBtn.addEventListener("click", ()=>{
         startGame()
-        // want to connect enemy move functions to start but still working on it
     })
 
     restartBtn.addEventListener("click",()=>{
@@ -110,8 +122,9 @@ function resetPlayer(){
 
 
     // On load 
-    moveEnemy1()
-    setInterval(moveEnemy1,(enemy1Speed * 10))
+    
+    moveEnemy1() // we call all the move functions first and then have them loop afterward using setinterval
+    setInterval(moveEnemy1,(enemy1Speed * 10)) // we move the enemy 10 times so multiplying the speed of those moves by 10 gives us a perfect loop 
     moveEnemy2()
     setInterval(moveEnemy2,(enemy2Speed * 10))
     moveEnemy3()
